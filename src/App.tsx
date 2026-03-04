5a4196105845461beb546b00561e724c8cb77bb1
Mikhail Koshelev <koshelev.mikhail@outlook.com>
2026-03-04 17:51:14 +0200

    Refactor App component to integrate Eta for dynamic HTML rendering and update index.html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
+import { useEffect, useRef, useState } from "react";
 import { Editor } from "@monaco-editor/react";
-import { Button } from "./components/ui/button";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "./components/ui/resizable";
+import { Eta } from "eta";
+
+const eta = new Eta();
 
 export function App() {
+  const frameRef = useRef<HTMLIFrameElement | null>(null);
+  const [template, setTemplate] = useState<string>("");
+
+  useEffect(() => {
+    if (!frameRef.current) return;
+
+    const updateFrame = async () => {
+      const html = eta.renderString(template, { model: {} });
+      frameRef.current!.srcdoc = html;
+    };
+    updateFrame();
+  }, [frameRef, template]);
+
   return (
-    <ResizablePanelGroup orientation="horizontal">
+    <ResizablePanelGroup orientation="horizontal" className="h-full">
       <ResizablePanel>
         <Editor
           height="90vh"
           defaultLanguage="html"
-          defaultValue="// some comment"
+          value={template}
+          onChange={(v) => setTemplate(v ?? "")}
           theme="vs-dark"
         />
       </ResizablePanel>
       <ResizableHandle withHandle />
       <ResizablePanel>
-        <Button>Hello world</Button>
+        <iframe ref={frameRef} className="bg-white h-full w-full"></iframe>
       </ResizablePanel>
     </ResizablePanelGroup>
   );
